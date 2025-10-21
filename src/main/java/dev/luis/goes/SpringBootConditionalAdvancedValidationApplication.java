128871c5c437de1bfbd9716c73a7cc21d7914909
Luis Gabriel Goés <luisgabriel533@gmail.com>
2025-10-21 18:23:49 -0300

    feat: Início do projeto de estudos

---
+++ src/main/java/dev/luis/goes/SpringBootConditionalAdvancedValidationApplication.java
@@ -0,0 +1,13 @@
+package dev.luis.goes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConditionalAdvancedValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConditionalAdvancedValidationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/luis/goes/SpringBootConditionalAdvancedValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.luis.goes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootConditionalAdvancedValidationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
